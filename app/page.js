08d97336c227321fe5044aa6620069cff79aa635
Yogesh Kshirsagar <yogeshkshirsagar2007@gmail.com>
2026-04-30 11:48:40 +0530

    add homepage

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>NeuraOne is Live 🚀</h1>
+
+      <p>Backend is working.</p>
+
+      <a href="/editor/123">Go to Editor</a>
+    </div>
+  );
+}
